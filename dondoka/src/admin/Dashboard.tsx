d3916d7e6d5ab018c68f7fa87d1e3ff66fcaa93f
PastorWilson <matmarques221@gmail.com>
2026-04-26 18:45:06 -0300

    melhorando o login

--- dondoka/src/admin/Dashboard.tsx
+++ dondoka/src/admin/Dashboard.tsx
@@ -1,24 +1,9 @@
 
 
 export default async function Dashboard() {
-
-    async function GetSizeProducts(params: type) {
-
-    }
-    async function GetSizeClientes(params: type) {
-
-    }
-    async function GetSize(params: type) {
-
-    }
-
-
     return (
         <>
-            <div className="container-grid-container">
-                <div className="container-products"></div>
-                <div className="container-products"></div>
-            </div>
+           
         </>
     )
 }
