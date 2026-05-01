bc22b31de86cd956357969ac97baa1340457f8af
PastorWilson <matmarques221@gmail.com>
2026-04-30 22:55:11 -0300

    atualizando

--- api/src/database/db.ts
+++ api/src/database/db.ts
@@ -4,7 +4,7 @@ import { Pool } from 'pg';
 const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || ,
+  password: process.env.DB_PASSWORD ,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
 });
